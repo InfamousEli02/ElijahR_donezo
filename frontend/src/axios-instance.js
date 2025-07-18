da8d62c0e8a138c97a4915c17c2da8ab32eaf34f
InfamousEli02 <ejreed328@gmail.com>
2025-07-18 18:54:28 -0400

    created code for setting up a new todo form

--- frontend/src/axios-instance.js
+++ frontend/src/axios-instance.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 import supabase from "./client";
 
+//function will get supabase session and attach the session.access_token to a new axios instance 
+// via a HTTP header
 const getAxiosClient = async () => {
   // Get the supabase session
   // create an axios instance with the supabase access_token
   // Return the instance
   // Fill code here
+  const currentSession = await supabase.auth.getSession();
+
+  const instance = axios.create({
+    headers: {
+      Authorization: `Bearer ${currentSession.data.session.access_token}`,
+    },
+  });
+
+//return the axios 'instance' to be used outside of the function
+return instance;
 };
 
 export default getAxiosClient;
