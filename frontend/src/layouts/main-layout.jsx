6392fbf425e84da06be1843721dcd075b64ea5ed
InfamousEli02 <ejreed328@gmail.com>
2025-07-18 16:44:59 -0400

    Did Auth Layout section of Donezo

--- frontend/src/layouts/main-layout.jsx
+++ frontend/src/layouts/main-layout.jsx
@@ -1,3 +1,22 @@
-export default function MainLayout(){
+import { Link, Outlet } from 'react-router-dom';
 
+export default function MainLayout(){
+  return (
+    //navigation bar for the login (link to homepage, button for logout)
+    //The Outlet component acts as a placeholder that dynamically renders the child route component 
+    // based on the current URL path
+    <>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">Donezo</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li><button className="btn btn-link">Logout</button></li>
+          </ul>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  )
 }
